6cf67c58ff83e23d996f9850e6fdaeb586be7f7d
sumancfc <sumanstha999@gmail.com>
2023-08-29 14:28:37 +0200

    python first program

---
+++ main.py
@@ -0,0 +1,63 @@
+# This is a python comment
+"""
+if 5 < 2:
+    print("Five is greater than 2");
+else:
+    print("2 is lesser than 5");
+"""
+
+# x = 5
+# y = "John"
+
+
+
+# Casting => Specify the data type of a variable
+# x = int(3)
+# y = str("John")
+#
+# print(type(x))
+# print(type(y))
+
+"""
+Python is case sensitive. i.e A and a are different
+variable names are case sensitive
+"""
+
+# Variable Names
+# myvar = "Suman"
+# my_var = "Suman"
+# first_name = "Suman"
+# firstName = "Suman"
+
+"""Multi Word Variable
+Camel Case: firstName
+Pascal Case: FirstName
+Snake Case: first_name
+"""
+
+# x, y, z = "Suman", "Shrestha", "Nepal";
+# print(x)
+# print(y)
+# print(z)
+#
+# x = y = z = "Shusman";
+# print(x)
+# print(y)
+# print(z)
+
+# Unpack a Collection
+# fruits = ["Apple",  "Orange", "Banana"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+#
+# # i = "Python is Awesome"
+# # print(i)
+#
+# i = 69
+# j = "is"
+# k = "Awesome"
+# print(i , j, k)
+
+
