7761402ee9c7b6cbbd0d480646f909da583e744f
sumancfc <sumanstha999@gmail.com>
2023-08-29 14:31:19 +0200

    python data types

---
+++ dataTypes.py
@@ -0,0 +1,29 @@
+# Built-in Data Types
+"""
+text type = str
+numeric types = int, float, complex
+sequence Types = list,tuple, range
+mapping type = dict
+set types = set, frozenset
+boolean type = bool
+binary types = bytes, bytearray, memoryview
+none type = NoneType
+"""
+# x= 5
+# print(type(x))
+
+# x = ["Apple", "Ball", "Cat", "Dog", "Egg"]; => this is list type
+# x = ("Apple", "Ball", "Cat", "Dog", "Egg"); => this is tuple type
+# x = range(5); => this is range type
+
+# x = {"name": "Suman", "age": 28} => this is dict type
+
+# x = {"Apple", "Ball", "Cat"} => this is set type
+# x = frozenset({"Apple", "Ball", "Cat"}) => this is set type
+
+# x = True => this is boolean type
+# x = b"Hello" => this is byte types
+x = None
+print(x);
+
+print(type(x))
